98dfafa726fda221d4c5ded42cde82693c84b164
Artem Penteskul <artemiy.penteskul@gmail.com>
2023-04-02 20:07:06 +0300

    added method to object

--- course/profile.js
+++ course/profile.js
@@ -4,7 +4,12 @@ const postsQty = 23
 const userProfile = {
     name,
     postsQty,
-    hasSignedAgreement: false
+    hasSignedAgreement: false,
+    sayHello: function () {
+        console.log('Hello!');
+    }
 }
 
 console.log(userProfile);
+
+userProfile.sayHello();
