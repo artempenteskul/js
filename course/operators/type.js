4af17b21ccdfbe000544a35307db9c72b621c192
Artem Penteskul <artemiy.penteskul@gmail.com>
2023-04-04 23:43:24 +0300

    added type module

---
+++ course/operators/type.js
@@ -0,0 +1,13 @@
+'use strict';
+
+console.log(typeof 10);
+console.log(typeof Boolean);
+console.log(typeof 10.4);
+console.log(typeof true);
+console.log(typeof console);
+console.log(typeof console.log);
+console.log(typeof undefined);
+console.log(typeof null);
+console.log(typeof '1232');
+console.log(typeof {test: 'test'});
+console.log(typeof [1, 3, 4]);
