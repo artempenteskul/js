13601fe379a1dee5c85121f5b38fbc3d4dd0010d
Artem Penteskul <artemiy.penteskul@gmail.com>
2023-04-06 20:40:02 +0300

    added defaultValue module

--- course/functions/defaultValue.js
+++ course/functions/defaultValue.js
@@ -4,3 +4,17 @@ function multByFactor(value, multiplier = 1) {
 
 console.log(multByFactor(5, 2));
 console.log(multByFactor(5));
+
+
+const multByFactor1 = function(value, multiplier = 1) {
+    return value * multiplier;
+}
+
+console.log(multByFactor1(5, 22));
+console.log(multByFactor(5));
+
+
+const multByFactor2 = (value, multiplier = 1) => value * multiplier;
+
+console.log(multByFactor2(55, 4));
+console.log(multByFactor2(5));
