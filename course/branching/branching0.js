91779894cf567d0a6f865b67dd5c48faed6404be
Artem Penteskul <artemiy.penteskul@gmail.com>
2023-04-07 21:31:53 +0300

    added branching module

---
+++ course/branching/branching0.js
@@ -0,0 +1,11 @@
+'use strict';
+
+let num = 9;
+
+if (num < 10) {
+    console.log('Value is less than 10.')
+} else if (num > 10) {
+    console.log('Value is greater than 10.')
+} else {
+    console.log('Value is equal to 10.')
+}
