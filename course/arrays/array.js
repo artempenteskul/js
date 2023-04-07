a78904398b26bcef753f94fe43b0b03ae8f01e0a
Artem Penteskul <artemiy.penteskul@gmail.com>
2023-04-07 18:31:25 +0300

    added array module

--- course/arrays/array.js
+++ course/arrays/array.js
@@ -1,5 +1,8 @@
 const myArray = [1, 2, 3];
 console.log(myArray);
 
-const myArray2 = new Array(1, 2, 3, 4);
+const myArray2 = new Array(1, 2, 3);
 console.log(myArray2);
+
+console.log(myArray == myArray2);
+console.log(myArray === myArray2);
