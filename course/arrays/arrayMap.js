448b1e34d974d0487c7dfd1a1bf2e0c81b1bca09
Artem Penteskul <artemiy.penteskul@gmail.com>
2023-04-07 19:20:54 +0300

    added arrayMap module

---
+++ course/arrays/arrayMap.js
@@ -0,0 +1,14 @@
+const myArray = [1, 3, 5];
+console.log(myArray);
+
+const newArray = myArray.map(el => el ** 3);
+
+console.log(newArray);
+console.log(myArray);
+
+// 
+
+const testArray = myArray.forEach(el => el ** 3);
+
+console.log(testArray);
+console.log(myArray);
