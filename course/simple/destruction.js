7b58d38e1bd00355b6bddaff3b95b4f138468144
Artem Penteskul <artemiy.penteskul@gmail.com>
2023-04-07 19:33:20 +0300

    added destruction module

---
+++ course/simple/destruction.js
@@ -0,0 +1,12 @@
+const userProfile = {
+    name: 'Artem',
+    commentsQty: 23,
+    hasSignedAgreement: false
+}
+
+const {name, commentsQty} = userProfile;
+const hasSignedAgreement = userProfile.hasSignedAgreement;
+
+console.log(name);
+console.log(commentsQty);
+console.log(hasSignedAgreement);
