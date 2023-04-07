f219ce84b175f33330f142d8560c2ba6cb72f4e5
Artem Penteskul <artemiy.penteskul@gmail.com>
2023-04-07 22:13:57 +0300

    added switch module

---
+++ course/branching/switch.js
@@ -0,0 +1,15 @@
+const month = 2;
+
+switch (month) {
+    case 12:
+        console.log('December');
+        break
+    case 1:
+        console.log('January');
+        break    
+    case 2: 
+        console.log('February');
+        break
+    default:
+        console.log('It is not winter month.');
+}
