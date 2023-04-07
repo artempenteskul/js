a89d99e3d619165f71e5acaf02a2897b57de21c4
Artem Penteskul <artemiy.penteskul@gmail.com>
2023-04-07 18:39:55 +0300

    added array2 module

---
+++ course/arrays/array2.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const myArray = [1, true, 'abc'];
+
+console.log(myArray);
+
+console.log(myArray[0]);
+console.log(myArray[2]);
+console.log(myArray[99]);
+
+console.log(myArray.length);
+
