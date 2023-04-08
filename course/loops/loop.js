06ddc7294bbc7b4ad46823aaca816b0cf94ccb8b
Artem Penteskul <artemiy.penteskul@gmail.com>
2023-04-08 15:30:14 +0300

    added for loop plus arrays example

--- course/loops/loop.js
+++ course/loops/loop.js
@@ -6,3 +6,12 @@
 for (let i = 0; i < 5; i++) {
     console.log(i);
 }
+
+
+// for loop plus array
+
+const myArray = ['first', 'second', 'third']
+
+for (let i = 0; i < myArray.length; i++) {
+    console.log(myArray[i]);
+}
