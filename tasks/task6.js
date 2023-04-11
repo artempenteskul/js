c8671e005a44d8c3e95e00297fb3b0bc20a0e444
Artem Penteskul <artemiy.penteskul@gmail.com>
2023-04-11 21:54:57 +0300

    solved task6

---
+++ tasks/task6.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const strVar = 'text';
+const intVar = 4;
+const boolVar = true;
+const nullVar = null;
+const undefinedVar = undefined;
+const objectVar = {};
+const listVar = [];
+
+console.log(typeof strVar);
+console.log(typeof intVar);
+console.log(typeof boolVar);
+console.log(typeof nullVar);
+console.log(typeof undefinedVar);
+console.log(typeof objectVar);
+console.log(typeof listVar);
