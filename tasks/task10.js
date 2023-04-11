fb6b499ea79efbdf4618c9525a98dfce0f9d8ad6
Artem Penteskul <artemiy.penteskul@gmail.com>
2023-04-12 00:26:52 +0300

    solved 9th and 10th tasks

---
+++ tasks/task10.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const differNumbers = function(a, b) {
+    return a - b;
+}
+
+console.log(differNumbers(432, 231));
+console.log(differNumbers(11, 32));
+console.log(differNumbers(2, 12));
---
+++ tasks/task9.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const sayHello = () => { return 'hello, world!' };
+
+const sayHello2 = () => 'hello, world!';
+
+
+console.log(sayHello());
+console.log(sayHello2());
