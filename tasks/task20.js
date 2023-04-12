4583250f6d20f44e98c4ff754e9dea6043810390
Artem Penteskul <artemiy.penteskul@gmail.com>
2023-04-12 18:32:31 +0300

    solved task20

---
+++ tasks/task20.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const currentDate = new Date();
+
+const currentDateInMs = currentDate.getTime();
+
+console.log(currentDateInMs);
+
+console.log(typeof currentDate);
+console.log(typeof currentDateInMs);
