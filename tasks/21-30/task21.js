4f7e970f2c0b09dee30b749d23f41fc8b8354fb6
Artem Penteskul <artemiy.penteskul@gmail.com>
2023-04-12 18:56:49 +0300

    solved task21

---
+++ tasks/21-30/task21.js
@@ -0,0 +1,13 @@
+'use strict';
+
+let myVar1 = '10';
+let myVar2 = 5;
+
+console.log(parseInt(myVar1) <= parseInt(myVar2));
+
+//
+
+myVar1 = '20';
+myVar2 = 100;
+
+console.log(+myVar1 <= +myVar2);
