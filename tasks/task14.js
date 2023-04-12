663c40b739a9d2e51bac0f6a41f4ab17c8411bcb
Artem Penteskul <artemiy.penteskul@gmail.com>
2023-04-12 18:19:22 +0300

    solved task14

---
+++ tasks/task14.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const randomStr = 'randomStr';
+
+console.log(randomStr instanceof String);
+console.log(typeof randomStr);
+
+const uppercaseStr = randomStr.toUpperCase();
+
+console.log(uppercaseStr);
