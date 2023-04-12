6d93f4b94158360a9ed1dec4b85f77bb1b3e4b7f
Artem Penteskul <artemiy.penteskul@gmail.com>
2023-04-12 18:12:05 +0300

    solved task12

---
+++ tasks/task12.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const info = {
+    name: 'Artem',
+    surname: 'Penteskul',
+    favNumber: 1
+}
+
+console.log(`My name is ${info.name} ${info.surname}, my fav number is - ${info.favNumber}.`)
