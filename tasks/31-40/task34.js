2b9d613f70cad07c1c972bcaafe7a3e9d48b39ef
Artem Penteskul <artemiy.penteskul@gmail.com>
2023-04-17 14:00:08 +0300

    solved task34

---
+++ tasks/31-40/task34.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const cars = [
+    {carBrand: 'BMW', price: 2000, isAvailableForSale: true},
+    {carBrand: 'Tesla', price: 2500, issAvailableForSale: false},
+    {carBrand: 'Volkswagen', price: 1500, isAvailableForSale: true}
+]
+
+cars.push({carBrand: 'Mercedes', price: 3000, isAvailableForSale: false});
+
+console.log(cars);
+console.log(cars.length);
