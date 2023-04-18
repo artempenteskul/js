a27be885cbc12d26ca0a21be064dd308d7c24b5b
Artem Penteskul <artemiy.penteskul@gmail.com>
2023-04-18 16:42:45 +0300

    solved task45

---
+++ tasks/41-50/task45.js
@@ -0,0 +1,32 @@
+'use strict';
+
+function isElementInArray(searchElement, inputArray) {
+    if (typeof searchElement !== 'object') {
+        return inputArray.includes(searchElement);
+    }
+
+    const strInputArray = inputArray.map(el => JSON.stringify(el));
+    return strInputArray.includes(JSON.stringify(searchElement));
+
+}
+
+const tags = [
+    ['javascript', 'es6'],
+    ['css', 'flexbox'],
+    ['html', 'web-browser'],
+]
+
+const fruits = [
+    { title: 'Orange', quantity: 10 },
+    { title: 'Banana', quantity: 5 },
+    { title: 'Apple', quantity: 25 },
+]
+  
+const primitiveTypesArray = [25, 'x', true, undefined, null]
+  
+console.log(isElementInArray(['css', 'flexbox'], tags)) // true  
+console.log(isElementInArray(['flexbox', 'css'], tags)) // false
+console.log(isElementInArray({ title: 'Apple', quantity: 25 }, fruits)) // true
+console.log(isElementInArray({ title: 'Banana' }, fruits)) // false
+console.log(isElementInArray(25, primitiveTypesArray)) // true
+  
