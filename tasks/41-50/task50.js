32f4c45eb3d6c2d9c85ed0ae10316b2fd9c04d9a
Artem Penteskul <artemiy.penteskul@gmail.com>
2023-04-18 17:17:45 +0300

    solved task50

---
+++ tasks/41-50/task50.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const formTemplateLiteral = num => {
+    return `Number ${num}
+This number is ${num >= 10 ? 'more or equal 10' : 'less than 10'}.
+The square root of this number is ${Math.sqrt(num)}.`
+}
+
+const myNumber = 9;
+console.log(formTemplateLiteral(myNumber));
+
+console.log('===')
+
+const myAnotherNumber = 25;
+console.log(formTemplateLiteral(myAnotherNumber));
