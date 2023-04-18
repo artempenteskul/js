09144a6f7085445a255a24f27b57d2c4f59f3208
Artem Penteskul <artemiy.penteskul@gmail.com>
2023-04-18 16:17:12 +0300

    solved task44

---
+++ tasks/41-50/task44.js
@@ -0,0 +1,13 @@
+'use strict';
+
+function isElementInArray(inputArray, searchElement) {
+    return inputArray.includes(searchElement)
+}
+
+const transports = ['Bus', 'Car', 'Bicycle', 'Airplane']
+
+
+
+console.log(isElementInArray(transports, 'Bus')) // true
+console.log(isElementInArray(transports, 'Phone')) // false
+console.log(isElementInArray(transports, 'Airplane')) // true
