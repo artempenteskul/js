00b2b863045061abff2c4d4430be230eb2e56ff4
Artem Penteskul <artemiy.penteskul@gmail.com>
2023-04-19 21:03:13 +0300

    solved task58

---
+++ tasks/51-60/task58.js
@@ -0,0 +1,17 @@
+'use strict';
+
+let person = {
+  _id: '5ad8cefcc0971792dacb3f1f',
+  index: 4,
+  processed: false,
+  cart: ['item1', 'item2', 'item3'],
+  email: 'slarsen@test.com',
+  name: 'Samanta Larsen',
+  cartId: 435,
+}
+
+delete person._id;
+delete person.processed;
+delete person.cart;
+
+console.log(person);
