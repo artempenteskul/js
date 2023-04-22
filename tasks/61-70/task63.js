5fbefb57bc0faec703646293f994ba7020305688
Artem Penteskul <artemiy.penteskul@gmail.com>
2023-04-22 15:12:26 +0300

    solved task63

---
+++ tasks/61-70/task63.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const b = 2
+let d = 15
+
+function myFn1(a) {
+  let b
+  let d = 10
+  myFn2(b)
+}
+
+function myFn2(a) {
+  let c = 5
+  console.log(a, b, c, d)
+}
+
+myFn1()
+// result - undefined, 2, 5, 15 -> there is no enclosing scope in js
