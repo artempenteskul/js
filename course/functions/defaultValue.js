defdd5f70ae6ab9ab994672498d6c9b54c3331cb
Artem Penteskul <artemiy.penteskul@gmail.com>
2024-01-09 21:48:33 +0200

    refactoring

--- course/functions/defaultValue.js
+++ course/functions/defaultValue.js
@@ -1,20 +1,20 @@
-function multByFactor(value, multiplier = 1) {
+function multiplyByFactor(value, multiplier = 1) {
     return value * multiplier;
 }
 
-console.log(multByFactor(5, 2));
-console.log(multByFactor(5));
+console.log(multiplyByFactor(5, 2));
+console.log(multiplyByFactor(5));
 
 
-const multByFactor1 = function(value, multiplier = 1) {
+const multiplyByFactor1 = function(value, multiplier = 1) {
     return value * multiplier;
 }
 
-console.log(multByFactor1(5, 22));
-console.log(multByFactor(5));
+console.log(multiplyByFactor1(5, 22));
+console.log(multiplyByFactor(5));
 
 
-const multByFactor2 = (value, multiplier = 1) => value * multiplier;
+const multiplyByFactor2 = (value, multiplier = 1) => value * multiplier;
 
-console.log(multByFactor2(55, 4));
-console.log(multByFactor2(5));
+console.log(multiplyByFactor2(55, 4));
+console.log(multiplyByFactor2(5));
