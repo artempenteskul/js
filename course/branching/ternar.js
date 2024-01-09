13bd189a5b49cda728770d61c670dc645fdd454e
Artem Penteskul <artemiy.penteskul@gmail.com>
2024-01-09 21:42:15 +0200

    added ternar example

--- course/branching/ternar.js
+++ course/branching/ternar.js
@@ -1,6 +1,10 @@
 const value1 = 0;
 const value2 = 13;
+const value3 = 33;
 
-res = value1 && value2 ? value1 + value2 : 0;
+// ternary explanation: statement ? true : false
+res = value1 && value2 ? value1 + value2 : 'no result';
+res2 = value2 && value3 ? value2 + value3 : 'no result';
 
 console.log(res);
+console.log(res2);
